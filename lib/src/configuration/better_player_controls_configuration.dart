6827875103a7a74fb1534eeb9680c1f18e5e84df
PiriMedya <168563783+PiriMedya@users.noreply.github.com>
2025-09-03 09:17:17 +0300

    Update better_player_controls_configuration.dart

--- lib/src/configuration/better_player_controls_configuration.dart
+++ lib/src/configuration/better_player_controls_configuration.dart
@@ -241,7 +241,7 @@ class BetterPlayerControlsConfiguration {
   factory BetterPlayerControlsConfiguration.theme(ThemeData theme) {
     return BetterPlayerControlsConfiguration(
       textColor: theme.textTheme.bodyMedium?.color ?? Colors.white,
-      iconsColor: theme.textTheme.button?.color ?? Colors.white,
+      iconsColor: theme.textTheme.labelLarge?.color ?? Colors.white,
     );
   }
 }
